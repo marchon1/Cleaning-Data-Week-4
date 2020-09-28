98858b2e6ab373a77c8f1ac1da351140bc635120
marchon1 <marc_hon@hotmail.com>
2020-09-28 10:46:57 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,6 @@
 Xtrain<-read.table("./UCI HAR Dataset/train/X_train.txt")
 subtrain<-read.table("./UCI HAR Dataset/train/subject_train.txt")
 Ytrain<-read.table("./UCI HAR Dataset/train/Y_train.txt")
-#Xtrainbacc<-read.table("./UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt")
-#Ytrainbacc<-read.table("./UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt")
-#Ztrainbacc<-read.table("./UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt")
-#Xtrainbgyr<-read.table("./UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt")
-#Ytrainbgyr<-read.table("./UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt")
-#Ztrainbgyr<-read.table("./UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt")
-#Xtraintacc<-read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt")
-#Ytraintacc<-read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt")
-#Ztraintacc<-read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt")
 features<-"./UCI HAR Dataset/features.txt"
 
 analysis <- rbind(Ytrain, Xtrain, subtrain)
@@ -27,4 +18,4 @@ analysis2 <- cbind(analysis, train.or.test = "test")
 combanalysis <- rbind(analysis, analysis2)
 combanalysis2 <- combanalysis[,grepl( "-mean|-std" , names( combanalysis ))]
 
-write.table(combanalysis2, row.name=FALSE)
+write.table(combanalysis2, file = "run_analysis.csv", sep=",", row.name=FALSE)
